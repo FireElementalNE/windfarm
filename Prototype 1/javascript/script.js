dcfcd58d9b752952508bc0663644f12ddf25be57
FireElementalNE <FireElemental.NE@gmail.com>
2014-04-17 12:17:40 -0400

    updated screen 2

--- Prototype 1/javascript/script.js
+++ Prototype 1/javascript/script.js
@@ -1,6 +1,4 @@
-function callMe() {
-	console.log("ALSO ready!");
-}
+var foggedElements = new Array();
 
 function starsEnter(el) {
 	if(el == 'research') {
@@ -20,7 +18,33 @@ function starsLeave(el) {
 	}
 }
 
+function initFoggedElements() {
+	temp = new Array();
+	temp[0] = $('.smallToolWindow');
+	temp[1] = $('#bg');
+	temp[2] = $('.infoWindow');
+	temp[3] = $('.toolbar');
+	return temp;
+}
+
+function foggify(el) {
+	el.foggy({
+		blurRadius: 3,
+		cssFilterSupport: true
+	});
+}
+
+function screen2() {
+	//$('.infoWindow').hide();
+
+	for(var i = 0; i < foggedElements.length; i++) {
+		foggify(foggedElements[i]);
+	}
+	$('.zoomWindMill').foggy(false);
+}
+
 $( document ).ready(function() {
 	$('.rate').append('/hr');
 	$('.money').prepend('$');
+	foggedElements = initFoggedElements();
 });
